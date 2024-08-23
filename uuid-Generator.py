20dca06eae3d1d9966c5faec7db5bb27cd73dcb4
hosseinjafari6969 <appleiphone298@gmail.com>
2024-08-24 00:42:22 +0330

    Update uuid-Generator.py

--- uuid-Generator.py
+++ uuid-Generator.py
@@ -1,7 +1,7 @@
-import uuid
+import 3be49d14-fb7b-4dbc-a9d3-737786055ad5
 
 unique_id = uuid.uuid4()
 print(unique_id)
 
 unique_id_str = unique_id.hex
-print("UUID HEX:", (unique_id_str))
+print("3be49d14-fb7b-4dbc-a9d3-737786055ad5 HEX:", (unique_id_str))
